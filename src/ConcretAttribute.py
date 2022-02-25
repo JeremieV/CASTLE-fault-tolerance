bbff82c97514266ed4358c0bce902cdb4a8d25fa
OriginalCleric <hadeswastaken@gmail.com>
2022-02-25 23:00:21 +0000

    Wrote the continous attribute class, almost complete waiting for DHG to be functional

--- src/ConcretAttribute.py
+++ src/ConcretAttribute.py
@@ -34,3 +34,52 @@ class _ContinousAttributes(Attribute):
         min = range[0]
         max = range[1]
         result = str(min)+"<=x<="+str(max)
+
+#private class
+#concret implementation of Attribute, represents catagorical attributes
+class _CategoricalAttributes(Attribute):
+    #TODO
+    DHG = None
+    LeftTraversal = None
+
+    def __init__(self,name,index):
+        self.name = name
+        self.index = index
+        self.LeftTraversal = self.DHG.getLeftTraversal() #TODO
+
+
+       
+    def createRange(self,values):
+        min = None
+        max = None
+        minRank = self.LeftTraversal.len() 
+        maxRank = -1
+        for element in values:
+            rank = self.LeftTraversal.index(element)
+            if (rank<minRank):
+                min = rank
+            elif(rank>maxRank):
+                max = rank
+
+        result = (min,max)
+        return result
+
+    def expandRange(self,range,value):
+        min = range[0]
+        max = range[1]
+        
+        minRank = self.LeftTraversal.index(min)
+        maxRank = self.LeftTraversal.index(max)
+        valueRank = self.LeftTraversal.index(value)
+
+        result = None
+        if (valueRank<minRank):
+            result = (value,max)
+        elif (valueRank>maxRank):
+            result = (min,value)
+        else:
+            result = range
+        return result
+
+    def getGeneralization(self, range):
+        return self.DHG.getLowestCommonAncestor(range) #TODO
