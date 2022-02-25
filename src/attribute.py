e8dbef0df3816fd3ea192278914fe235fcaa3525
OriginalCleric <hadeswastaken@gmail.com>
2022-02-25 16:46:03 +0000

    Added comments to abstract attribute

--- src/attribute.py
+++ src/attribute.py
@@ -3,14 +3,17 @@ import tuple_obj
 
 class Attribute(ABC):
 
+    #returns true if this attribute is a quasi-identifier
     @abstractmethod
     def isQI():
         return False
 
+    #return the value of this attribute in the given tuple
     @abstractmethod
-    def getValue(tuple_obj):
+    def getValue(tuple):
         return NotImplementedError
 
+    #returns the generalized value of this attribute according to given range
     @abstractmethod
     def getGeneralization(range):
         return NotImplementedError
