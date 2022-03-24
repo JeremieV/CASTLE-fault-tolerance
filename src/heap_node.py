6f7a6e32cd863bf05681c87c6349c26f9f53a7d9
Tim Page <tim.page@warwick.ac.uk>
2022-03-24 01:58:58 +0000

    fixed heapnode init

--- src/heap_node.py
+++ src/heap_node.py
@@ -6,9 +6,12 @@ class HeapNode:
     def __init__(self, t=None, distance=None):
         if t is not None:
             self.tuple: tuple = t
-        
+        else:
+            self.tuple: tuple
         if distance is not None:
             self.distance: int = distance
+        else:
+            self.distance: int = sys.maxsize
     
     def set_tuple(self, t, distance):
         self.tuple = t
